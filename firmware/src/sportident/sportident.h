20993558e7a67c7c1083ed8797d60caee076f1e6
Oliver Larsson <larsson.e.oliver@gmail.com>
2023-12-27 17:04:21 +0100

    Card number decoding

--- firmware/src/sportident/sportident.h
+++ firmware/src/sportident/sportident.h
@@ -5,6 +5,11 @@
 typedef struct {
 } si_reader_t;
 
+typedef struct {
+  int length;
+  uint8_t data[];
+} si_command_t;
+
 // Returns the number of bytes in the command written to outbuf, -1 on error.
 int get_command(const si_reader_t *const reader, uint8_t command, uint8_t *argv,
                 int argc, uint8_t outbuf[]);
